c8a2ed37de24fe7dbffb332d2dc42b18ea130e80
apple <apple@apples-MacBook-Pro-2.local>
2020-02-12 19:07:32 -0800

    periodic save 3

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,15 @@
-export const userLocalStorage (key, initialValue) => {
-        const [storedValue, setStoredValue] = useState(() => {
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+         const [storedValue, setStoredValue] = useState(() => {
           const item = window.localStorage.getItem(key);
           return item ? JSON.parse(item) : initialValue;
         });
+        const setValue = value => {
+            setStoredValue(value);
+            window.localStorage.setItem(key, JSON.stringify(value));
+        };
+
+
+        return [storedValue];
 };
